325e31477b149e3ada3dbf37d5d6081f991040ff
Palak Jain <palakj1@uci.edu>
2020-08-04 12:03:44 -0700

    created system that allows for programmatic adding cardviews
    Signed-off-by: Palak JAin <palakj1@uci.edu>

--- app/src/main/java/com/sum20/whisp/ui/resources/ResourcesFragment.kt
+++ app/src/main/java/com/sum20/whisp/ui/resources/ResourcesFragment.kt
@@ -1,14 +1,20 @@
 package com.sum20.whisp.ui.resources
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sum20.whisp.R
+
 
 class ResourcesFragment : Fragment() {
 
@@ -22,10 +28,23 @@ class ResourcesFragment : Fragment() {
         resourcesViewModel =
             ViewModelProvider(this).get(ResourcesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_resources, container, false)
-        val textView: TextView = root.findViewById(R.id.textView2)
-        resourcesViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = root.findViewById(R.id.textView2)
+//        resourcesViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
+        val parent = root.findViewById<LinearLayout>(R.id.cat_lin)
+        val resCatList = resources.getStringArray(R.array.resource_cats)
+
+
+        for (i in 0..2) {
+            val custom: View = inflater.inflate(R.layout.resources_categories, null)
+            val tv = custom.findViewById<View>(R.id.text) as TextView
+            tv.text = resCatList[i]
+            parent.addView(custom)
+//            parent.s
+        }
+
         return root
     }
 }
